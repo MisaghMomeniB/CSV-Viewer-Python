e7195800144f6ad22a0ecc09fa87061c08e4ebbf
MisaghMomeniB <jenabemisagh@gmail.com>
2025-03-31 09:50:47 +0330

    import librarys

---
+++ src/main.py
@@ -0,0 +1,8 @@
+import sys
+import csv
+from PyQt5.QtWidgets import (
+    QApplication, QMainWindow, QTableView, QFileDialog, 
+    QVBoxLayout, QWidget, QPushButton, QLabel, QStatusBar
+)
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QStandardItemModel, QStandardItem
